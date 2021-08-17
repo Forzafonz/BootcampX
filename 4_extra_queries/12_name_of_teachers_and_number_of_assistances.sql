1606621706d92e3fa5d36d5086dc60673f79c5a8
Anton <golubin1990@gmail.com>
2021-08-17 18:33:39 -0400

    Added a new query

---
+++ 4_extra_queries/12_name_of_teachers_and_number_of_assistances.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests.*)
+FROM assistance_requests JOIN teachers
+ON assistance_requests.teacher_id = teachers.id
+JOIN students
+ON assistance_requests.student_id = students.id
+JOIN cohorts
+ON students.cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teachers.name;
