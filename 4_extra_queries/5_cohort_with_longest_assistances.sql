ede081f1ee8ac5b6324d84f87f2efbab3a4fff3a
Anton <golubin1990@gmail.com>
2021-08-17 17:54:03 -0400

    New query

---
+++ 4_extra_queries/5_cohort_with_longest_assistances.sql
@@ -0,0 +1,7 @@
+SELECT max(average_assistance_time ) as average_assistance_time
+FROM
+(SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time 
+FROM cohorts JOIN students ON cohorts.id = students.cohort_id
+JOIN assistance_requests ON students.id = assistance_requests.student_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time) w;
