9a17fdf6edfb6dbcceacc12c786b90c117f6d67e
Anton <golubin1990@gmail.com>
2021-08-17 17:41:25 -0400

    A new query

---
+++ 4_extra_queries/4_average_cohort_assistance_time.sql
@@ -0,0 +1,5 @@
+SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time 
+FROM cohorts JOIN students ON cohorts.id = students.cohort_id
+JOIN assistance_requests ON students.id = assistance_requests.student_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time;
