98f2300821f6c14abe7f5d1060ce260945260210
Anton <golubin1990@gmail.com>
2021-08-17 16:57:44 -0400

    Added two additional tables to the database

--- migrations/student_cohorts.sql
+++ migrations/student_cohorts.sql
@@ -4,6 +4,8 @@ DROP TABLE IF EXISTS assignments CASCADE;
 DROP TABLE IF EXISTS assignment CASCADE;
 DROP TABLE IF EXISTS assignment_submissions CASCADE;
 DROP TABLE IF EXISTS assignment_submission CASCADE;
+DROP TABLE IF EXISTS teachers CASCADE;
+DROP TABLE IF EXISTS assistance_requests CASCADE;
 
 CREATE TABLE cohorts(
   id SERIAL PRIMARY KEY NOT NULL,
@@ -39,3 +41,25 @@ CREATE TABLE assignment_submissions (
   duration INTEGER,
   submission_date DATE
 );
+
+
+CREATE TABLE teachers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  start_date DATE,
+  end_date DATE,
+  is_active BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE assistance_requests(
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+  created_at TIMESTAMP,
+  started_at TIMESTAMP,
+  completed_at TIMESTAMP,
+  student_feedback TEXT,
+  teacher_feedback TEXT
+);
+
